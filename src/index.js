9b5b7107235f03b7f90bbb07778f642f1dfa98e4
JADAV ANIKET KAMLESHKUMAR <aniketjadav2002@gmail.com>
2023-09-26 11:40:32 +0530

    js added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-window.addEventListener("scroll", () => {
-  var timeline = document.getElementById("notimeline");
-  if (window.scrollY > 1500) {
-    // console.log("HI");
-
-    timeline.classList.add("timeline");
-  } else {
-    timeline.classList.remove("timeline");
-  }
-});
-
 function darkTheme() {
   var element = document.body;
+  var mover = document.getElementById("img1");
   element.classList.toggle("dark-mode");
 
   var contact = document.getElementById("contact");
   contact.classList.toggle("dark-contact");
   contact.classList.toggle("contact");
+  mover.classList.toggle("mover");
 }
+
+function sidebarActivate() {
+  var sidebartoggle = document.querySelector(".navb");
+  sidebartoggle.classList.add("navb_activate");
+}
+function sidebarDeActivate() {
+  var sidebartoggle = document.querySelector(".navb");
+  sidebartoggle.classList.remove("navb_activate");
+}
+
+// Remove Menu Mobile
+
+// const navLink = document.querySelectorAll(".nav__link");
+
+// function linkAction() {
+//    navMenu = document.getElementById("nav-menu");
+//   // When we click on each nav__link, we remove the show-menu class
+//   navMenu.classList.remove("show-menu");
+// }
+// navLink.forEach((n) => n.addEventListener("click", linkAction));
